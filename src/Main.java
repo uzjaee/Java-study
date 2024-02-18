85cda0723317da43d17df3dd2b77c36244168d99
uzjaee <kyj960802@gmail.com>
2024-02-18 23:08:28 +0900

    ADD : Static 사용 방법

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        StaticExercise s1 = new StaticExercise();
+        StaticExercise s2 = new StaticExercise();
+        s1.name = "Lee";
+        System.out.println("s1 name = " + s1.name);
+        System.out.println("s2 name = " + s2.name);
+        System.out.println(System.identityHashCode(s1.name));
+        System.out.println(System.identityHashCode(s2.name));
+
+        NotstaticExercise s3 =  new NotstaticExercise();
+        NotstaticExercise s4 = new NotstaticExercise();
+
+        s3.name = "Choi";
+        System.out.println("s3 name = " + s3.name);
+        System.out.println("s4 name = "  + s4.name);
+        System.out.println(System.identityHashCode(s3.name));
+        System.out.println(System.identityHashCode(s4.name));
+
+
+    }
+}
---
+++ src/StaticExercise.java
@@ -0,0 +1,5 @@
+public class StaticExercise {
+      static  String name = "kim";
+
+}
+
